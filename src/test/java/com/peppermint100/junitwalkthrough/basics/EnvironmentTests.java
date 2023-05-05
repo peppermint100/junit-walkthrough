e1b2f411dd8804f8976bcf7efa531e9b903fefc8
peppermint100 <krpeppermint100@gmail.com>
2023-05-05 16:12:30 +0900

    JRE 테스트

--- src/test/java/com/peppermint100/junitwalkthrough/basics/EnvironmentTests.java
+++ src/test/java/com/peppermint100/junitwalkthrough/basics/EnvironmentTests.java
@@ -3,8 +3,7 @@ package com.peppermint100.junitwalkthrough.basics;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.condition.EnabledOnOs;
-import org.junit.jupiter.api.condition.OS;
+import org.junit.jupiter.api.condition.*;
 
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
@@ -12,7 +11,7 @@ import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
 @Slf4j
-public class OSTests {
+public class EnvironmentTests {
     @Target(ElementType.METHOD)
     @Retention(RetentionPolicy.RUNTIME)
     @Test
@@ -37,6 +36,24 @@ public class OSTests {
     @TestOnMac
     void onlyOnMacWithCustomAnnotation() {
         log.info("Test for mac with custom annotation");
+        Assertions.assertEquals(1, 1);
+    }
+
+    @Test
+    @EnabledOnJre({ JRE.JAVA_11 })
+    void onlyForJava11() {
+        Assertions.assertEquals(1, 1);
+    }
+
+    @Test
+    @EnabledOnJre({ JRE.JAVA_17 })
+    void onlyForJava17() {
+        Assertions.assertEquals(1, 2);
+    }
+
+    @Test
+    @DisabledForJreRange(min = JRE.JAVA_9, max = JRE.JAVA_11)
+    void notFromJava9to11() {
         Assertions.assertEquals(1, 1);
     }
 }
